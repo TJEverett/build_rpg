dad8296d77ed12d67e81e80d22f3b8bea4ea4950
Tristen Everett <spock214@gmail.com>
2020-06-23 11:35:59 -0700

    correctly randomize stats of new characters

--- src/character.js
+++ src/character.js
@@ -5,4 +5,15 @@ export class Character {
     this.def = 0;
     this.spd = 0;
   }
+
+  create(specialty) {
+    if (specialty === "attack"){
+      this.atk += 1;
+    } else if (specialty === "defense"){
+      this.def += 1;
+    }
+    this.atk += (2 + Math.ceil(Math.random() * 3));
+    this.def += Math.ceil(Math.random() * 3);
+    this.spd += (Math.ceil(Math.random() * 30) / 10);
+  }
 }

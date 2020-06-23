8cd76b96222c8078588613ee3b6e70732e064474
Tristen Everett <spock214@gmail.com>
2020-06-23 11:15:48 -0700

    create test for randomizing stats of created characters

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -8,4 +8,19 @@ describe('Character', () => {
     expect(hero.def).toEqual(0);
     expect(hero.spd).toEqual(0);
   });
+
+  test('should create 4 heros with different stats', () => {
+    let hero1 = new Character("hero1");
+    hero1.create("attack");
+    let hero2 = new Character("hero2");
+    hero2.create("attack");
+    let hero3 = new Character("hero3");
+    hero3.create("defense");
+    let hero4 = new Character("hero4");
+    hero4.create("defense");
+    console.log(hero1);
+    console.log(hero2);
+    console.log(hero3);
+    console.log(hero4);
+  });
 });

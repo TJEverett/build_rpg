bf11de73ae8a7379c72ea8db3bef85741fddbfc2
Tristen Everett <spock214@gmail.com>
2020-07-01 11:31:21 -0700

    create the turn prototype so that it passes all 4 related tests

--- src/battle.js
+++ src/battle.js
@@ -14,4 +14,32 @@ export class Battle {
       this.player.hpCurrent -= ((this.computer.atk * 2) - this.player.def);
     }
   }
+
+  turn(){
+    if(this.player.hpCurrent > 0 && this.computer.hpCurrent > 0){
+      if(this.player.spd > this.computer.spd){
+        this.damage("attack");
+        if(this.computer.hpCurrent > 0){
+          this.damage("defend");
+        }
+      }else if (this.computer.spd > this.player.spd){
+        this.damage("defend");
+        if(this.player.hpCurrent > 0){
+          this.damage("attack");
+        }
+      }else{
+        this.damage("attack");
+        this.damage("defend");
+      }
+      if(this.player.hpCurrent <= 0){
+        return "you lose";
+      }else if (this.computer.hpCurrent <= 0){
+        return "you win";
+      }else{
+        return "turn complete";
+      }
+    }else{
+      return "a character is already dead";
+    }
+  }
 }
